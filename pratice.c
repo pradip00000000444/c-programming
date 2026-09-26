fe0604c19fe75db5dcfc16428006ac8962d09026
pradip Bhat <pradipbhat150@gmail.com>
2026-09-26 21:06:24 +0545

    use of scanf in advance and  use of getchar.

--- pratice.c
+++ pratice.c
@@ -1,7 +1,12 @@
 #include<stdio.h> 
 int  main() { 
 int num1;
+char ch; 
+printf("enter an integer value\n");
 scanf("%d",&num1);
-printf("%d\n",num1);
+getchar();// after integer, if the character the printing value is not show  to enter the value. should use getchar(), use space before format specifier. 
+printf("enter an characte value\n");
+scanf("%c",&ch);
+printf("character is: %c , integer is: %d",ch,num1);
   return 0;
 }
