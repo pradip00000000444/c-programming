a728b388e4ab8b63fd5ba0e86b78f1107a763515
pradip Bhat <pradipbhat150@gmail.com>
2026-09-26 20:10:59 +0545

    use of scanf

--- sum.c
+++ sum.c
@@ -1,10 +1,8 @@
 #include<stdio.h> 
 int main () {
-int a = 10, n= 12; 
-printf("sum 0f a and n is : %d\n",(a+n));
-printf("sub of a and n is :%d \n",(a-n));
-printf("multi of a and n is: %d\n",(a*n));
-printf("div of a and n is: %d\n",(a/n));
+int a; 
+scanf("%d",a);
+
   return 0;
 }
   
