38ffec3ee1dabd5a86b0934a99347bc903937654
pradip Bhat <pradipbhat150@gmail.com>
2026-09-26 11:06:33 +0545

    okay basics

--- hello.c
+++ hello.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
-
-
 int main(){
-  printf("hello world");
-  return 0;
-}
+ printf("hell0 \"pradip\"");
+ printf("\nhello\"harshit\"");
+ printf("percentage=%");
+  // int var; // varaible intilization 
+  // var= 22; // varaibke decelaration  
+  // int first_name = 22; // intilization and delecration in single line.
+  // char g = 'a';
+  // long i = 22233; 
+  // double d =222.3333;
+  // float f = 2.000; 
+  // // char last_name ='pradip';
+  // printf("%d\n",first_name); // printing 
+  // printf("%c\n", g); 
+  // printf("double=%.3lf\n", d);
+  //  printf ("float=%.2f\n", f); 
+  // // printf("%c",last_name);
+  return 0; 
+} 
+
+ 
